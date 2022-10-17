ed58fe8afa6ad0b7d307640d7ff78984816e9a04
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 10:53:03 -0600

    Complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    if session[:page_views] < 3
+      article = Article.find(params[:id])
+      session[:page_views] += 1
+      render json: article 
+    else 
+      session[:page_views] += 1
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end 
   end
 
   private
